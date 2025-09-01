82d42391831b98b5d51a6a1e6d0e6b7fd379bac2
nhat.pham-van <embeddedrtos.vietnam@gmail.com>
2025-09-01 13:02:03 +0700

    update : add author in post

--- script/posts.py
+++ script/posts.py
@@ -1,9 +1,17 @@
 import os
 import requests
 import json
+import re
 
 ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
 PAGE_ID = os.environ.get("PAGE_ID")
+
+# Load authors.json
+with open("authors/authors.json", "r", encoding="utf-8") as f:
+    authors_data = json.load(f)
+
+# Convert authors list to dict for quick lookup
+authors_dict = {author["id"]: author for author in authors_data}
 
 url = f'https://graph.facebook.com/v23.0/{PAGE_ID}/posts'
 params = {
@@ -18,6 +26,8 @@ data = response.json()
 news_posts = []
 posts_posts = []
 
+id_pattern = re.compile(r"#([A-Z]\d{3})", re.IGNORECASE)
+
 # Print results and classify by hashtag
 for post in data.get("data", []):
     message = post.get("message", "")
@@ -27,7 +37,15 @@ for post in data.get("data", []):
     print("🖼 Image:", post.get("full_picture", "No image"))
     print("-----")
 
-    # Check hashtags in the message (convert to lowercase for consistency)
+    author_info = None
+    match = id_pattern.search(message)
+    if match:
+        author_id = match.group(1).upper()  
+        if author_id in authors_dict:
+            author_info = authors_dict[author_id]
+            post["author"] = author_info
+
+    # Check hashtags in message
     if "#news" in message.lower():
         news_posts.append(post)
     if "#posts" in message.lower():
