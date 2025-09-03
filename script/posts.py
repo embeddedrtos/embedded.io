94756646c0d99973f270d97ba79d924739ca45d7
nhat.pham-van <embeddedrtos.vietnam@gmail.com>
2025-09-03 23:38:55 +0700

    update posts.py

--- script/posts.py
+++ script/posts.py
@@ -15,7 +15,7 @@ authors_dict = {author["id"]: author for author in authors_data}
 
 url = f'https://graph.facebook.com/v23.0/{PAGE_ID}/posts'
 params = {
-    'fields': 'id,message,created_time,permalink_url,full_picture,insights.metric(post_impressions)',
+    'fields': 'id,message,created_time,permalink_url,full_picture',
     'access_token': ACCESS_TOKEN
 }
 
@@ -33,16 +33,6 @@ ca_category_pattern = re.compile(r"#CA([A-Za-z0-9_]+)", re.IGNORECASE)
 # Print results and classify by hashtag
 for post in data.get("data", []):
     message = post.get("message", "")
-    views = None
-    
-    # Get views from insights
-    insights = post.get("insights", {}).get("data", [])
-    for metric in insights:
-        if metric.get("name") == "post_impressions":
-            views = metric.get("values", [{}])[0].get("value", 0)
-            break
-
-    post["views"] = views if views is not None else 0
 
     # Check author id pattern (#A001, #B123...)
     author_info = None
@@ -56,7 +46,7 @@ for post in data.get("data", []):
     # Check CA + CATEGORY
     category_match = ca_category_pattern.search(message)
     if category_match:
-        category = category_match.group(1)  
+        category = category_match.group(1)
         post["category"] = category
 
     # Debug print
@@ -64,7 +54,6 @@ for post in data.get("data", []):
     print("📅 Date:", post["created_time"])
     print("🔗 Link:", post["permalink_url"])
     print("🖼 Image:", post.get("full_picture", "No image"))
-    print("👀 Views:", post["views"])
     if "category" in post:
         print("🏷 Category:", post["category"])
     print("-----")
